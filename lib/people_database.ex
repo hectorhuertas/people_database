a7c2c5ce4cc86b6029dea809b5ae9f680359a189
Hector Huertas <hectorhuertas@gmail.com>
2016-02-16 13:48:55 -0700

    Start with person list + get people

--- lib/people_database.ex
+++ lib/people_database.ex
@@ -1,28 +1,44 @@
 defmodule PeopleDatabase do
   def start(people \\ []) do
     IO.puts("Program started")
-    IO.puts(people)
-    initialize_database(people)
+    initialize_database
+    load(people)
   end
 
-  def load(list) do
-
+  def load(people) do
+    Enum.each(people, fn person -> PeopleDatabase.add(person) end)
   end
 
-  def initialize_database(people) when is_list(people) do
+  def add(%Person{} = person) do
+    Agent.update(:database, fn list -> [person|list] end)
+  end
+
+  def initialize_database do
+    Agent.start_link(fn -> [] end, name: :database)
     IO.puts("Database initialized")
-    Agent.start_link(fn -> people end, name: :database)
-    msg = Agent.get(:database, fn list -> list end)
-    IO.puts(msg)
   end
 
   def get_data do
     list = Agent.get(:database, fn list -> list end)
-    IO.puts(list)
+    # IO.puts(List.to_string(list))
     list
   end
+  # def initialize_database(people) when is_list(people) do
+  #   Agent.start_link(fn -> people end, name: :database)
+  #   IO.puts("Database initialized")
+  #   # msg = Agent.get(:database, fn list -> list end)
+  #   # IO.puts(msg)
+  # end
 
-  def add(person) do
-    Agent.update(:database, fn list -> [person|list] end)
-  end
+  # def initialize_database(people) do
+  #   IO.puts("Param must be a list")
+  # end
+
+  # def add(%Person{} = person) do
+  #   Agent.update(:database, fn list -> [person|list] end)
+  # end
+  # def add(person) do
+  #   IO.puts("Didn't match")
+  #   Agent.update(:database, fn list -> [person|list] end)
+  # end
 end
